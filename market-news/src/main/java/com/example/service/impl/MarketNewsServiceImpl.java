1edf5d47d09af2a285bbcd8614a134077607d3d8
Nicolas_Wu <nicolas_wu@epcnszxw0265.princeton.epam.com>
2016-08-19 15:48:43 +0800

    Improve codes

--- market-news/src/main/java/com/example/service/impl/MarketNewsServiceImpl.java
+++ market-news/src/main/java/com/example/service/impl/MarketNewsServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.service.impl;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.response.MarketNewsResponse;
 import com.example.service.IMarketNewsService;
@@ -9,9 +10,12 @@ import com.example.service.IMarketNewsService;
 @Service
 public class MarketNewsServiceImpl implements IMarketNewsService {
 
+	@Autowired
+	private MarketNewsResponse marketNewsResponse;
+
 	@Override
 	public MarketNewsResponse retrieveMarketNews(Date date) {
-		return null;
+		return marketNewsResponse;
 	}
 
 }
